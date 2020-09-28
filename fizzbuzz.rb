75638b1c0269728e5cfab7ca884b82d296a1147f
Marc Steele <marcsteele2014@gmail.com>
2020-09-28 20:54:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if(
-    if int % 3 == 0
-      "Fizz"
-    end
-    &&
-    if int % 5 == 0
-      "Buzz"
-    end
-    ) == true
-    "FizzBuzz"
+  if int % 3 == 0
+    "Fizz"
   end
+  if int % 5 == 0
+    "Buzz"
+  end 
 end
